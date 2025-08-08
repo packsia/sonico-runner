aa04c10b249f8c1aafb2a1b313f3326de66c9e07
packsia <cait@packsia.com>
2025-08-07 17:40:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,283 +1,230 @@
 'use strict';
 
-/********************************
- * IMAGE LOADING
- ********************************/
-const IMG = {
-    runnerIdle: loadImage('assets/sonico-images/sonicoman.png'),
-    runnerRun: loadImage('assets/sonico-images/sonicoman.gif'),
-    floor: loadImage('assets/sonico-images/floor.png'),
-    cloud: loadImage('assets/sonico-images/cloud.png'),
-    bird1: loadImage('assets/sonico-images/bird1.png'),
-    bird2: loadImage('assets/sonico-images/bird2.png'),
-    martini: [
-        loadImage('assets/sonico-images/martini1.png'),
-        loadImage('assets/sonico-images/martini2.png'),
-        loadImage('assets/sonico-images/martini3.png'),
-        loadImage('assets/sonico-images/martini4.png')
-    ],
-    palm: [
-        loadImage('assets/sonico-images/palm1.png'),
-        loadImage('assets/sonico-images/palm2.png'),
-        loadImage('assets/sonico-images/palm3.png'),
-        loadImage('assets/sonico-images/palm4.png')
-    ],
-    gameover: loadImage('assets/sonico-images/gameover.png'),
-    restart: loadImage('assets/sonico-images/restart.png')
-};
-
+/**************
+ * IMAGE LOADER
+ **************/
 function loadImage(src) {
-    const img = new Image();
-    img.src = src;
-    return img;
+  const img = new Image();
+  img.src = src;
+  return img;
 }
 
-/********************************
- * CONFIG
- ********************************/
-const TrexConfig = {
-    WIDTH: 78,
-    HEIGHT: 88,
-    START_X_POS: 50,
-    JUMP_VELOCITY: -10,
-    GRAVITY: 0.6
+const IMG = {
+  runnerIdle: loadImage('assets/sonico-images/sonicoman.png'),
+  runnerRun: loadImage('assets/sonico-images/sonicoman.gif'),
+  floor: loadImage('assets/sonico-images/floor.png'),
+  cloud: loadImage('assets/sonico-images/cloud.png'),
+  bird: [loadImage('assets/sonico-images/bird1.png'), loadImage('assets/sonico-images/bird2.png')],
+  martini: [
+    loadImage('assets/sonico-images/martini1.png'),
+    loadImage('assets/sonico-images/martini2.png'),
+    loadImage('assets/sonico-images/martini3.png'),
+    loadImage('assets/sonico-images/martini4.png')
+  ],
+  palm: [
+    loadImage('assets/sonico-images/palm1.png'),
+    loadImage('assets/sonico-images/palm2.png'),
+    loadImage('assets/sonico-images/palm3.png'),
+    loadImage('assets/sonico-images/palm4.png')
+  ],
+  gameover: loadImage('assets/sonico-images/gameover.png'),
+  restart: loadImage('assets/sonico-images/restart.png')
 };
 
-const CloudConfig = {
-    WIDTH: 60,
-    HEIGHT: 30,
-    MIN_GAP: 100,
-    MAX_GAP: 400
+/**************
+ * CONFIG
+ **************/
+const TrexConfig = {
+  WIDTH: 78,
+  HEIGHT: 88,
+  X: 50,
+  JUMP_VELOCITY: -10,
+  GRAVITY: 0.6
 };
 
 const ObstacleTypes = [
-    { name: 'MARTINI', width: 40, height: 60, yPos: 105, images: IMG.martini, minGap: 120 },
-    { name: 'PALM', width: 50, height: 70, yPos: 90, images: IMG.palm, minGap: 120 },
-    { name: 'BIRD', width: 46, height: 40, yPos: [100, 75, 50], images: [IMG.bird1, IMG.bird2], minGap: 150, frameRate: 6 }
+  { images: IMG.martini, width: 40, height: 60, yPos: 90 },
+  { images: IMG.palm, width: 50, height: 70, yPos: 75 },
+  { images: IMG.bird, width: 46, height: 40, yPos: 70, frameRate: 6 }
 ];
 
-/********************************
+/**************
  * TREX
- ********************************/
+ **************/
 class Trex {
-    constructor(canvasCtx) {
-        this.canvasCtx = canvasCtx;
-        this.xPos = TrexConfig.START_X_POS;
-        this.yPos = 0;
-        this.groundYPos = 150 - TrexConfig.HEIGHT; // ground level
-        this.status = 'WAITING';
-        this.jumpVelocity = 0;
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.x = TrexConfig.X;
+    this.y = 150 - TrexConfig.HEIGHT;
+    this.groundY = 150 - TrexConfig.HEIGHT;
+    this.status = 'IDLE';
+    this.jumpVel = 0;
+  }
+  startJump() {
+    if (this.status !== 'JUMPING') {
+      this.status = 'JUMPING';
+      this.jumpVel = TrexConfig.JUMP_VELOCITY;
     }
-
-    startJump() {
-        if (this.status !== 'JUMPING') {
-            this.status = 'JUMPING';
-            this.jumpVelocity = TrexConfig.JUMP_VELOCITY;
-        }
+  }
+  update() {
+    if (this.status === 'JUMPING') {
+      this.y += this.jumpVel;
+      this.jumpVel += TrexConfig.GRAVITY;
+      if (this.y >= this.groundY) {
+        this.y = this.groundY;
+        this.status = 'RUNNING';
+      }
     }
-
-    update() {
-        if (this.status === 'JUMPING') {
-            this.yPos += this.jumpVelocity;
-            this.jumpVelocity += TrexConfig.GRAVITY;
-            if (this.yPos >= this.groundYPos) {
-                this.yPos = this.groundYPos;
-                this.status = 'RUNNING';
-            }
-        }
-    }
-
-    draw() {
-        const img = (this.status === 'RUNNING') ? IMG.runnerRun : IMG.runnerIdle;
-        this.canvasCtx.drawImage(img, this.xPos, this.yPos, TrexConfig.WIDTH, TrexConfig.HEIGHT);
-    }
+  }
+  draw() {
+    const img = (this.status === 'RUNNING') ? IMG.runnerRun : IMG.runnerIdle;
+    this.ctx.drawImage(img, this.x, this.y, TrexConfig.WIDTH, TrexConfig.HEIGHT);
+  }
+  getBounds() {
+    return { x: this.x, y: this.y, width: TrexConfig.WIDTH, height: TrexConfig.HEIGHT };
+  }
 }
 
-/********************************
- * CLOUD
- ********************************/
-class Cloud {
-    constructor(canvasCtx, xPos, yPos) {
-        this.canvasCtx = canvasCtx;
-        this.xPos = xPos;
-        this.yPos = yPos;
-    }
-    update(speed) {
-        this.xPos -= speed;
-    }
-    draw() {
-        this.canvasCtx.drawImage(IMG.cloud, this.xPos, this.yPos, CloudConfig.WIDTH, CloudConfig.HEIGHT);
-    }
-}
-
-/********************************
+/**************
  * OBSTACLE
- ********************************/
+ **************/
 class Obstacle {
-    constructor(canvasCtx, typeIndex) {
-        this.canvasCtx = canvasCtx;
-        this.typeConfig = ObstacleTypes[typeIndex];
-        this.currentFrame = 0;
+  constructor(ctx, type) {
+    this.ctx = ctx;
+    this.type = type;
+    this.frame = 0;
+    this.frameTimer = 0;
+    this.x = 800;
+    this.y = type.yPos;
+  }
+  update(speed, delta) {
+    this.x -= speed;
+    if (this.type.frameRate) {
+      this.frameTimer += delta;
+      if (this.frameTimer >= 1000 / this.type.frameRate) {
+        this.frame = (this.frame + 1) % this.type.images.length;
         this.frameTimer = 0;
-        this.size = 1;
-        this.xPos = 800;
-        this.yPos = this.typeConfig.yPos;
+      }
     }
-    update(speed, deltaTime) {
-        this.xPos -= speed;
-        if (this.typeConfig.frameRate) {
-            this.frameTimer += deltaTime;
-            if (this.frameTimer >= 1000 / this.typeConfig.frameRate) {
-                this.currentFrame = (this.currentFrame + 1) % this.typeConfig.images.length;
-                this.frameTimer = 0;
-            }
-        }
-    }
-    draw() {
-        const img = this.typeConfig.images[this.currentFrame % this.typeConfig.images.length];
-        this.canvasCtx.drawImage(img, this.xPos, this.yPos, this.typeConfig.width * this.size, this.typeConfig.height);
-    }
+  }
+  draw() {
+    const img = this.type.images[this.frame % this.type.images.length];
+    this.ctx.drawImage(img, this.x, this.y, this.type.width, this.type.height);
+  }
+  getBounds() {
+    return { x: this.x, y: this.y, width: this.type.width, height: this.type.height };
+  }
 }
 
-/********************************
+/**************
  * FLOOR
- ********************************/
-class HorizonLine {
-    constructor(canvasCtx, dimensions) {
-        this.canvasCtx = canvasCtx;
-        this.dimensions = dimensions;
-        this.xPos = [0, dimensions.WIDTH];
-        this.yPos = dimensions.HEIGHT - 20;
-    }
-    update(speed) {
-        this.xPos[0] -= speed;
-        this.xPos[1] -= speed;
-        if (this.xPos[0] <= -this.dimensions.WIDTH) {
-            this.xPos[0] = this.xPos[1] + this.dimensions.WIDTH;
-        }
-        if (this.xPos[1] <= -this.dimensions.WIDTH) {
-            this.xPos[1] = this.xPos[0] + this.dimensions.WIDTH;
-        }
-    }
-    draw() {
-        this.canvasCtx.drawImage(IMG.floor, this.xPos[0], this.yPos, this.dimensions.WIDTH, 20);
-        this.canvasCtx.drawImage(IMG.floor, this.xPos[1], this.yPos, this.dimensions.WIDTH, 20);
-    }
+ **************/
+class Horizon {
+  constructor(ctx, w) {
+    this.ctx = ctx;
+    this.w = w;
+    this.x = [0, w];
+  }
+  update(speed) {
+    this.x[0] -= speed;
+    this.x[1] -= speed;
+    if (this.x[0] <= -this.w) this.x[0] = this.x[1] + this.w;
+    if (this.x[1] <= -this.w) this.x[1] = this.x[0] + this.w;
+  }
+  draw() {
+    this.ctx.drawImage(IMG.floor, this.x[0], 130, this.w, 20);
+    this.ctx.drawImage(IMG.floor, this.x[1], 130, this.w, 20);
+  }
 }
 
-/********************************
- * GAME OVER PANEL
- ********************************/
-class GameOverPanel {
-    constructor(canvasCtx, canvasDimensions) {
-        this.canvasCtx = canvasCtx;
-        this.canvasDimensions = canvasDimensions;
-    }
-    draw() {
-        const centerX = this.canvasDimensions.WIDTH / 2;
-        this.canvasCtx.drawImage(IMG.gameover, centerX - (IMG.gameover.width / 2), this.canvasDimensions.HEIGHT / 3);
-        this.canvasCtx.drawImage(IMG.restart, centerX - (IMG.restart.width / 2), this.canvasDimensions.HEIGHT / 2);
-    }
-}
-
-/********************************
- * MAIN GAME
- ********************************/
+/**************
+ * GAME
+ **************/
 class Game {
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.ctx = canvas.getContext('2d');
-        this.dimensions = { WIDTH: canvas.width, HEIGHT: canvas.height };
-        this.horizon = new HorizonLine(this.ctx, this.dimensions);
-        this.trex = new Trex(this.ctx);
-        this.clouds = [];
-        this.obstacles = [];
-        this.gameOverPanel = new GameOverPanel(this.ctx, this.dimensions);
-        this.speed = 6;
-        this.isPlaying = false;
-        this.lastTime = null;
-        this.bindEvents();
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.trex = new Trex(this.ctx);
+    this.horizon = new Horizon(this.ctx, this.width);
+    this.obstacles = [];
+    this.speed = 6;
+    this.isPlaying = false;
+    this.lastTime = null;
+    this.bindEvents();
+    this.renderIdleScreen();
+  }
+  bindEvents() {
+    document.addEventListener('keydown', e => {
+      if (e.code === 'Space') {
+        if (!this.isPlaying) this.start();
+        else this.trex.startJump();
+      }
+    });
+    const mobileBtn = document.getElementById('mobile-btn');
+    mobileBtn.addEventListener('click', () => {
+      if (!this.isPlaying) {
+        this.start();
+        mobileBtn.textContent = 'Jump';
+      } else {
+        this.trex.startJump();
+      }
+    });
+  }
+  start() {
+    this.isPlaying = true;
+    this.trex.status = 'RUNNING';
+    this.lastTime = performance.now();
+    requestAnimationFrame(this.update.bind(this));
+  }
+  end() {
+    this.isPlaying = false;
+    document.getElementById('mobile-btn').textContent = 'Start';
+    this.renderIdleScreen();
+  }
+  update(timestamp) {
+    const delta = timestamp - this.lastTime;
+    this.lastTime = timestamp;
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.horizon.update(this.speed);
+    this.horizon.draw();
+    this.trex.update();
+    this.trex.draw();
+    this.updateObstacles(delta);
+    if (this.isPlaying) requestAnimationFrame(this.update.bind(this));
+  }
+  updateObstacles(delta) {
+    if (Math.random() < 0.02) {
+      const type = ObstacleTypes[Math.floor(Math.random() * ObstacleTypes.length)];
+      this.obstacles.push(new Obstacle(this.ctx, type));
     }
-
-    bindEvents() {
-        document.addEventListener('keydown', (e) => {
-            if (e.code === 'Space') {
-                if (!this.isPlaying) {
-                    this.start();
-                }
-                this.trex.startJump();
-            }
-        });
-        this.canvas.addEventListener('click', () => {
-            if (!this.isPlaying) {
-                this.start();
-            }
-            this.trex.startJump();
-        });
-    }
-
-    start() {
-        this.isPlaying = true;
-        this.trex.status = 'RUNNING';
-        this.lastTime = performance.now();
-        requestAnimationFrame(this.update.bind(this));
-    }
-
-    update(timestamp) {
-        const deltaTime = timestamp - this.lastTime;
-        this.lastTime = timestamp;
-
-        // Clear screen
-        this.ctx.clearRect(0, 0, this.dimensions.WIDTH, this.dimensions.HEIGHT);
-
-        // Update game elements
-        this.horizon.update(this.speed);
-        this.horizon.draw();
-
-        this.trex.update();
-        this.trex.draw();
-
-        this.updateClouds(deltaTime);
-        this.updateObstacles(deltaTime);
-
-        // Loop game
-        if (this.isPlaying) {
-            requestAnimationFrame(this.update.bind(this));
-        } else {
-            this.gameOverPanel.draw();
-        }
-    }
-
-    updateClouds(deltaTime) {
-        if (Math.random() < 0.005) {
-            this.clouds.push(new Cloud(this.ctx, this.dimensions.WIDTH, Math.random() * 50));
-        }
-        this.clouds.forEach(cloud => {
-            cloud.update(this.speed / 2);
-            cloud.draw();
-        });
-        this.clouds = this.clouds.filter(cloud => cloud.xPos + CloudConfig.WIDTH > 0);
-    }
-
-    updateObstacles(deltaTime) {
-        if (Math.random() < 0.02) {
-            const typeIndex = Math.floor(Math.random() * ObstacleTypes.length);
-            this.obstacles.push(new Obstacle(this.ctx, typeIndex));
-        }
-        this.obstacles.forEach(obstacle => {
-            obstacle.update(this.speed, deltaTime);
-            obstacle.draw();
-        });
-        this.obstacles = this.obstacles.filter(o => o.xPos + o.typeConfig.width > 0);
-    }
+    this.obstacles.forEach(o => {
+      o.update(this.speed, delta);
+      o.draw();
+      if (this.checkCollision(this.trex.getBounds(), o.getBounds())) {
+        this.end();
+      }
+    });
+    this.obstacles = this.obstacles.filter(o => o.x + o.type.width > 0);
+  }
+  checkCollision(r, o) {
+    return !(r.x > o.x + o.width || r.x + r.width < o.x || r.y > o.y + o.height || r.y + r.height < o.y);
+  }
+  renderIdleScreen() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.horizon.draw();
+    this.trex.status = 'IDLE';
+    this.trex.draw();
+  }
 }
 
-/********************************
- * START GAME
- ********************************/
+/**************
+ * INIT
+ **************/
 window.onload = function() {
-    const canvas = document.getElementById('game');
-    canvas.width = 800;
-    canvas.height = 150;
-    new Game(canvas);
+  const canvas = document.getElementById('game');
+  canvas.width = 800;
+  canvas.height = 150;
+  new Game(canvas);
 };
